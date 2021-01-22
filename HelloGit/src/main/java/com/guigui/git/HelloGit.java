0cb2842e6d8ac4658d0bad580e09675d3e954e26
MACHENIKE <957522005@qq.com>
2021-01-22 15:00:15 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/guigui/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.guigui.git;
+
+/**
+ * @author wzcstart
+ * @date 2021/1/22 - 14:47
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
